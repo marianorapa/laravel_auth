4f98243d99e2b043a4f3fc03f61460b83f0243ce
Mariano Rapaport <marianr99@outlook.com>
2020-04-03 12:38:54 -0300

    controlador de permisos devuelve vistas

--- app/Http/Controllers/PermisoController.php
+++ app/Http/Controllers/PermisoController.php
@@ -13,7 +13,7 @@ class PermisoController extends Controller
      */
     public function index()
     {
-        
+        return view('admin.permisos.index');
     }
 
     /**
@@ -23,7 +23,7 @@ class PermisoController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.permisos.create');
     }
 
     /**
