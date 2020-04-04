f08ef29faf7baff3e9a61d8f95b54c4f0c543e3a
Mariano Rapaport <marianr99@outlook.com>
2020-04-03 23:00:35 -0300

    Agrego carpeta de vistas de error

---
+++ resources/views/error/notAllowed.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container text-center">
+        <h1 class="h1 mt-5">No tiene autorización para acceder al recurso solicitado.</h1>
+    </div>
+@endsection
