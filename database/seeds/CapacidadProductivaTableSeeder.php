3870e1b8bc8ebaf6f62e333ddb4d03a09e4002f2
naninho205 <cgferrarotti@gmail.com>
2020-05-28 23:05:18 -0300

    Fix CapacidadProductivaTableSeeder.php

--- database/seeds/CapacidadProductivaTableSeeder.php
+++ database/seeds/CapacidadProductivaTableSeeder.php
@@ -12,43 +12,43 @@ class CapacidadProductivaTableSeeder extends Seeder
      */
     public function run()
     {
-        
+
 
         \DB::table('capacidad_productiva')->delete();
-        
+
         \DB::table('capacidad_productiva')->insert(array (
-            0 => 
+            0 =>
             array (
                 'id' => 1,
                 'capacidad' => 135000,
                 'fecha_desde' => '2011-05-23',
                 'fecha_hasta' => NULL,
-                'prioridad_id' => 1,
+                'prioridad_id' => 2,
                 'created_at' => '2020-05-23 05:25:45',
                 'updated_at' => '2020-05-23 05:25:45',
             ),
-            1 => 
+            1 =>
             array (
                 'id' => 2,
                 'capacidad' => 70000,
                 'fecha_desde' => '2013-05-10',
                 'fecha_hasta' => '2013-05-10',
-                'prioridad_id' => 2,
+                'prioridad_id' => 1,
                 'created_at' => '2020-05-23 05:25:45',
                 'updated_at' => '2020-05-23 05:25:45',
             ),
-            2 => 
+            2 =>
             array (
                 'id' => 3,
                 'capacidad' => 270000,
                 'fecha_desde' => '2015-06-23',
                 'fecha_hasta' => NULL,
-                'prioridad_id' => 1,
+                'prioridad_id' => 2,
                 'created_at' => '2020-05-23 05:25:45',
                 'updated_at' => '2020-05-23 05:25:45',
             ),
         ));
-        
-        
+
+
     }
 }
